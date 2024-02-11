00da0cb824387574058a8a1eacc74a9d6545e3bc
Munir <smsirajulmonir@gmail.com>
2024-02-11 15:37:45 +0600

    dashboard implemented

---
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -0,0 +1,10 @@
+
+const Cart = () => {
+    return (
+        <div>
+            <h2>This is dashboard</h2>
+        </div>
+    );
+};
+
+export default Cart;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -9,6 +9,8 @@ import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
 import Secret from "../Pages/Shared/Secret/Secret";
 import PrivateRoute from "./PrivateRoute";
+import Dashboard from "../Layout/Dashboard";
+import Cart from "../Pages/Dashboard/Cart/Cart";
 
   export const router = createBrowserRouter([
     {
@@ -40,5 +42,15 @@ import PrivateRoute from "./PrivateRoute";
               element:<PrivateRoute><Secret></Secret></PrivateRoute>
             }
         ]
+    },
+    {
+      path:'dashboard',
+      element:<Dashboard></Dashboard>,
+      children:[
+        {
+          path:'cart',
+          element:<Cart></Cart>
+        }
+      ]
     }
   ])
